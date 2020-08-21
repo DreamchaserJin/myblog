64883ab8d0ba6f4459ab2ffba6950a570827c38d
追梦 <767564319@qq.com>
2020-08-21 09:34:07 +0800

    优化展示效果，完善浏览数功能

--- src/main/java/com/dreamchaser/Application.java
+++ src/main/java/com/dreamchaser/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-
+//当有多个数据源时就得这样设置
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @MapperScan("com.dreamchaser.mapper")
 public class Application {
